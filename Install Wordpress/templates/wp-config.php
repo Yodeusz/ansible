c14ba057c5ef08d27aa58cd2b62446f97b61bf57
EC2 Default User <ec2-user@ip-172-31-29-126.eu-west-1.compute.internal>
2018-11-19 22:39:21 +0000

    some minor changes

--- Install Wordpress/templates/wp-config.php
+++ Install Wordpress/templates/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_database');
+define('DB_NAME', '{{project_name}}');
 
 /** MySQL database username */
 define('DB_USER', 'root');
